6fd1bb3df4f7a68a36aa93c8faad1158f774f0b6
TorbsT <torbst2001@gmail.com>
2021-10-16 21:02:23 +0200

    consecutive views
    added functionality for clicking start button multiple times. each starts a new view, so config may be edited before clicking again

--- Talgdat/Assets/Controller.cs
+++ Talgdat/Assets/Controller.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [System.Serializable]
 public class Controller
 {
-    private AudioSource AudioSource { get => Talgdat.AudioSource; }
-    public float uiDelay { get => _config.VisualSortTime / _commands.Count; }
+    public static Controller Instance { get; private set; }
+    public AudioSource AudioSource { get => Talgdat.AudioSource; }
+    
     private Core _core;
     private List<Bar> _bars = new List<Bar>();
     [SerializeField] private int _replayIndex;
@@ -17,26 +18,15 @@ public class Controller
     private List<Command> _commands;
     private ProblemConfig _config;
 
+    private SortView _sortView;
     private float _lastFrameTime;
     private float _timeSinceLastFrame;
-    private bool _active;
-
     public Controller()
     {
-
+        Instance = this;
     }
     public void StartButtonClicked()
     {
-        // START reset previous stuff
-        foreach (Bar bar in _bars)
-        {
-            BarPool.Instance.Enpool(bar);
-        }
-        _bars = new List<Bar>();
-        _unmarkQueue = new Queue<Bar>();
-        _activeMarks = 0;
-        // END
-
         _core = new Core();
 
         _config = new ProblemConfig
@@ -52,100 +42,19 @@ public class Controller
         };
 
         _core.Restart(_config);
-        Debug.Log(_config);
-        _bars = new List<Bar>();
-        for (int i = 0; i < _core.list.Count; i++)
-        {
-            int value = _core.list[i];
-            Bar bar = BarPool.Instance.Depool();
-            bar.index = i;
-            bar.value = value;
-            _bars.Add(bar);
-        }
-
         _core.Solve();
-        _active = true;
-        _lastFrameTime = Time.time;
+
+
+
+        if (_sortView != null) _sortView.Destroy();
+        _sortView = new SortView(_core.list, _config);
+        _sortView.Start();
     }
     public void Update()
     {
-        if (!_active) return;
-        _commands = _core.list.commands;
-        int counter = 0;
-        _timeSinceLastFrame = Time.time - _lastFrameTime;
-        if (_timeSinceLastFrame < uiDelay) return;
-        while (_timeSinceLastFrame >= uiDelay && _replayIndex < _commands.Count)
-        {
-            _timeSinceLastFrame -= uiDelay;
-            ShowScreen();
-            counter++;
-        }
-        _lastFrameTime = Time.time;
+        if (_sortView != null) _sortView.Update();
     }
-    private void ShowScreen()
-    {
-        _timeSinceLastDisplay = Time.time - _lastDisplayTime;
-        
-        if (_replayIndex >= _commands.Count) return;
-        Command command = _commands[_replayIndex];
-        if (command is SetIndexToValueCommand setCommand)
-        {
-            int barIndex = setCommand.index;
-            int barValue = setCommand.value;
 
-            Bar bar = _bars[barIndex];
-            bar.value = barValue;
-            Mark(barIndex, "write");
-        }
-        else if (command is SwapCommand swapCommand)
-        {
-            int aIndex = swapCommand.aIndex;
-            int bIndex = swapCommand.bIndex;
-            Bar aBar = _bars[aIndex];
-            Bar bBar = _bars[bIndex];
-
-            _bars[aIndex] = bBar;
-            bBar.index = aIndex;
-            _bars[bIndex] = aBar;
-            aBar.index = bIndex;
-            Mark(aIndex, "write");
-            Mark(bIndex, "write");
-        }
-        else if (command is ReadIndexCommand readCommand)
-        {
-            int index = readCommand.index;
-            float pitch = readCommand.value / 1080f + 0.5f;
-            Mark(index, "read");
-            PlaySound(index, pitch);
-        }
-
-
-
-        _replayIndex++;
-        _lastDisplayTime = Time.time;
-        //if (_replayIndex >= commands.Count) _replayIndex = 0;
-    }
-    private void Mark(int index, string tag)
-    {
-        Bar bar = _bars[index];
-        if (bar.ActiveWrites == 0) _activeMarks++;
-        bar.Mark(tag);
-        _unmarkQueue.Enqueue(bar);
-        if (_activeMarks > _config.UnmarkQueueSize)
-        {
-            Bar b = _unmarkQueue.Dequeue();
-            b.Unmark();
-            if (bar.ActiveWrites == 0) _activeMarks--;
-        }
-    }
-    private void PlaySound(int index, float pitch)
-    {
-        //Bar bar = _bars[index];
-        //bar.AudioSource.pitch = pitch;
-        //bar.AudioSource.Play();
-        AudioSource.pitch = pitch;
-        AudioSource.Play();
-    }
     public void FixedUpdate()
     {
 
--- Talgdat/Assets/IntList.cs
+++ Talgdat/Assets/IntList.cs
@@ -12,35 +12,68 @@ public class IntList
         get => get(index);
         set { set(index, value); }
     }
-    public List<int> elements { get => _elements; }
-    public int Count { get => _elements.Count; }
+    public int Count { get => _output.Count; }
 
-    private List<int> _elements;
+    private List<int> _input;
+    private List<int> _output;
     private List<Command> _commands;
     private int _arrayAccesses;
     private int _swaps;
+    private bool _inputLocked;
 
     public IntList()
     {
-        _elements = new List<int>();
+        _input = new List<int>();
         _commands = new List<Command>();
     }
     public void Swap(int a, int b)
     {
         if (OutOfRange(a) || OutOfRange(b)) throw new IndexOutOfRangeException("Swapping Indexes " + a + " & " + b + ", Count was " + Count);
         _swaps++;
-        int aValue = _elements[a];
-        int bValue = _elements[b];
-        _elements[a] = bValue;
-        _elements[b] = aValue;
+        int aValue = _output[a];
+        int bValue = _output[b];
+        _output[a] = bValue;
+        _output[b] = aValue;
         _commands.Add(new SwapCommand(a, aValue, b, bValue));
     }
-
+    public void LockInput()
+    {
+        if (_inputLocked) throw new InvalidOperationException("Input already locked");
+        _output = GetCopy(false);
+        _inputLocked = true;
+    }
+    public List<Command> CopyCommands()
+    {
+        if (!_inputLocked) throw new InvalidOperationException("Can't get commands, input isn't locked");
+        List<Command> result = new List<Command>();
+        foreach (Command c in _commands)
+        {
+            result.Add(c);
+        }
+        return result;
+    }
+    public List<int> GetInputCopy() => GetCopy(false);
+    public List<int> GetOutputCopy() => GetCopy(true);
+    public List<int> GetCopy(bool getOutput)
+    {
+        List<int> result = new List<int>();
+        foreach (int i in Get(getOutput))
+        {
+            result.Add(i);
+        }
+        return result;
+    }
+    private List<int> Get(bool getOutput)
+    {
+        if (getOutput && !_inputLocked) throw new InvalidOperationException("Can't get output when input isn't locked");
+        if (getOutput) return _output;
+        return _input;
+    }
     private int get(int index)
     {
         if (OutOfRange(index)) throw new IndexOutOfRangeException("Getting Index was " + index + ", Count was " + Count);
         _arrayAccesses++;
-        int value = _elements[index];
+        int value = _output[index];
         _commands.Add(new ReadIndexCommand(index, value));
         return value;
     }
@@ -48,7 +81,7 @@ public class IntList
     {
         if (OutOfRange(index)) throw new IndexOutOfRangeException("Setting Index was " + index + ", Count was " + Count);
         _arrayAccesses++;
-        _elements[index] = value;
+        _output[index] = value;
         _commands.Add(new SetIndexToValueCommand(index, value));
     }
     private bool OutOfRange(int index) => index < 0 || index >= Count;
@@ -60,18 +93,18 @@ public class IntList
         Random r = new Random(seed);
         for (int i = 0; i < count; i++)
         {
-            list._elements.Add(r.Next(min, max + 1));
+            list._input.Add(r.Next(min, max + 1));
         }
         return list;
     }
     public override string ToString()
     {
         string txt = "[";
-        for (int i = 0; i < _elements.Count-1; i++)
+        for (int i = 0; i < _output.Count-1; i++)
         {
-            txt += _elements[i].ToString()+", ";
+            txt += _output[i].ToString()+", ";
         }
-        txt += _elements[Count-1].ToString()+"]";
+        txt += _output[Count-1].ToString()+"]";
         if (_arrayAccesses > 0 || _swaps > 0)
         {
             txt += " with " + _arrayAccesses + " accesses and " + _swaps + " swaps";
